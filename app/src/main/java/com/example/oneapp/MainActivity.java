e0201e729e39e6ec019da3b016652c223c0cab01
Your Name <v_dennis@126.com>
2020-08-13 17:17:01 +0800

    add login layout

--- app/src/main/java/com/example/oneapp/MainActivity.java
+++ app/src/main/java/com/example/oneapp/MainActivity.java
@@ -21,24 +21,34 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName2);
-        String message = editText.getText().toString();
-        if (message.isEmpty()) {
-            Context context = getApplicationContext();
-            CharSequence text = "不能发送空白消息!";
+        EditText userId = findViewById(R.id.editTextTextPersonName);
+        String userId_value = userId.getText().toString();
+        EditText password = findViewById(R.id.editTextTextPassword2);
+        String password_value = password.getText().toString();
+        Context context = getApplicationContext();
+        if (userId_value.isEmpty()) {
+            CharSequence text = "请输入用户名!";
             int duration = Toast.LENGTH_SHORT;
 
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
-        } else {
-            Context context = getApplicationContext();
-            CharSequence text = "发送成功!";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
-            intent.putExtra(EXTRA_MESSAGE, message);
-            startActivity(intent);
+            return;
         }
+
+        if (password_value.isEmpty()) {
+            CharSequence text = "请输入用户密码!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+        CharSequence text = "发送成功!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        intent.putExtra(EXTRA_MESSAGE, userId_value);
+        startActivity(intent);
     }
 }
